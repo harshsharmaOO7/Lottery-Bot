53d0437c6525fc0ed1f965d08f6237b22fdb5bba
harshsharmaOO7 <79790245+harshsharmaOO7@users.noreply.github.com>
2026-04-01 23:41:31 +0530

    Add draw detection function and update result parsing

--- parser.py
+++ parser.py
@@ -1,9 +1,25 @@
 import datetime
 
-def parse_nagaland(data):
+def detect_draw(text):
+    text = text.lower()
+
+    if "1 pm" in text:
+        return "1PM"
+    elif "6 pm" in text:
+        return "6PM"
+    elif "8 pm" in text:
+        return "8PM"
+
+    return "unknown"
+
+
+def parse_result(data):
     return {
         "result": data["title"],
+        "draw": detect_draw(data["title"]),
         "image": data["image"],
         "date": str(datetime.date.today()),
-        "pdf": ""
+        "source": data["source"],
+        "pdf": "",
+        "verified": False
     }
